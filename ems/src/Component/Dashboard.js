6bed729a2f4ce2b2a236932e4d17a0ec2db67b6f
nadeem-zenithgodigital <nadeem.hasan@zenithgodigital.com>
2023-04-26 18:31:52 +0530

    Dashboard page added

--- ems/src/Component/Dashboard.js
+++ ems/src/Component/Dashboard.js
@@ -1,14 +1,129 @@
 import React from 'react'
 import SidebarMenu from './SidebarMenu'
-import { Container } from 'react-bootstrap'
+import { FcPieChart, FcReadingEbook, FcInspection } from "react-icons/fc";
+import { NavLink } from 'react-router-dom';
 
 function Dashboard() {
     return (
         <>
             <SidebarMenu />
-            <Container>
-                <h1>Hello Dashboard hiiSSS</h1>
-            </Container>
+            <div className="container-fluid">
+                <div className="row text-center">
+
+                    <div className="col-6 mt-2 col-md-2 mt-md-5">
+                    </div>
+
+
+
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <NavLink to="/dashboard">
+                            <div className="shadow text-white card p-2" style={{ backgroundColor: "#00306e" }}>
+                                <div className="d-flex align-items-center">
+                                    <div className="m-3 w-100">
+                                        <h4 className="mb-0 mt-0 textLeft"><FcPieChart /> </h4>
+                                        <span className="text-center">Dashbord</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </NavLink>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <NavLink to="/student">
+                            <div className="shadow bg-success text-white card p-2">
+                                <div className="d-flex align-items-center">
+                                    <div className="m-3 w-100">
+                                        <h4 className="mb-0 mt-0 textLeft"><FcReadingEbook /> </h4>
+                                        <span className="text-center">Student</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </NavLink>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow text-white card p-2" style={{ backgroundColor: "#1188a6" }}>
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcInspection /> </h4>
+                                    <span className="text-center">Exam</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow bg-dark text-white card p-2">
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcInspection /> </h4>
+                                    <span className="text-center">Final Exam</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow bg-warning text-white card p-2">
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcPieChart /> </h4>
+                                    <span className="text-center">Dashbord</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-5">
+                    </div>
+
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow text-white card p-2" style={{ backgroundColor: "#3BC6AC" }}>
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcPieChart /> </h4>
+                                    <span className="text-center">Dashbord</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow bg-success text-white card p-2">
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcPieChart /> </h4>
+                                    <span className="text-center">Dashbord</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow text-white card p-2" style={{ backgroundColor: "#1188a6" }}>
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcPieChart /> </h4>
+                                    <span className="text-center">Dashbord</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow bg-dark text-white card p-2">
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcPieChart /> </h4>
+                                    <span className="text-center">Dashbord</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 mt-2 col-md-2 mt-md-4">
+                        <div className="shadow bg-warning text-white card p-2">
+                            <div className="d-flex align-items-center">
+                                <div className="m-3 w-100">
+                                    <h4 className="mb-0 mt-0 textLeft"><FcPieChart /> </h4>
+                                    <span className="text-center">Dashbord</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
