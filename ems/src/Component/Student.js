719e13d3ae903c14f10fb322c49138689eab58f6
nadeem-zenithgodigital <nadeem.hasan@zenithgodigital.com>
2023-04-28 18:23:14 +0530

    Student page Added

--- ems/src/Component/Student.js
+++ ems/src/Component/Student.js
@@ -1,10 +1,240 @@
 import React from 'react'
+import { Row, Col, Table, Button } from "react-bootstrap"
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+
 
 const Student = () => {
     return (
-        <div>
-            <h1>hii Student page</h1>
-        </div>
+        <>
+            <div className='list'>
+                <Row>
+                    <Col>
+                        <h2>Student List</h2>
+                    </Col>
+                    <Col style={{ textAlign: "right" }}>
+                        <Button style={{ backgroundColor: "#1188A6", fontFamily: "'Montserrat', sans-serif" }}>Add Student</Button>&nbsp;
+                        <MdDelete color="#DC3545" fontSize="2.8em" />
+                    </Col>
+                </Row>
+            </div >
+
+
+            <div className='tableList'>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <th>Student ID</th>
+                            <th>Name</th>
+                            <th>City</th>
+                            <th>Selery</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>1</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><input type="checkbox" /></th>
+                            <td>Last</td>
+                            <td>Nadeem Hasan</td>
+                            <td>Noida</td>
+                            <td>Selery</td>
+                            <td>
+                                <FiEdit color="green" fontSize="1.5em" />&nbsp;
+                                <MdDelete color="#DC3545" fontSize="1.9em" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </>
     )
 }
 
