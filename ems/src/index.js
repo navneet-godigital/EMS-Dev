3724de6c1e6c7f9c1ca23da43e2c1e65981bc79b
raja-zenithgodigital <raja.kumar@zenithgodigital.com>
2023-05-16 14:43:37 +0530

    .

--- ems/src/index.js
+++ ems/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
-import * as ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
